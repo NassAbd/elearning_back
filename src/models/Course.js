6e4d3efe65a5faddae5af0a041ce8b99893cb31c
Abdallah Nassur <nassur1607@gmail.com>
2025-03-08 16:03:26 +0100

    add image to course model

--- src/models/Course.js
+++ src/models/Course.js
@@ -5,7 +5,8 @@ const Schema = mongoose.Schema;
 const courseSchema = new Schema({
   title: { type: String, required: true },
   description: { type: String, required: true },
-  content: { type: String, required: true }
+  content: { type: String, required: true },
+  imageUrl: { type: String, required: false }
 });
 
 module.exports = mongoose.model('Course', courseSchema);
